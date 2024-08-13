47e16c932a6375ba2a5847122ac1ae6cfd3e6a8e
dan <md.erhenede@gmail.com>
2024-08-14 05:14:59 +0800

    bb

--- client/src/Screens/Map.tsx
+++ client/src/Screens/Map.tsx
@@ -1,53 +1,79 @@
+import { SendDiagonal } from 'iconoir-react'
 import CustomMap from '../Components/CustomMap'
+import { CircleIconButtons } from '../Components/ui/Buttons'
 import { fetchCoordinates } from '../hooks/fetchCordinates'
-import { SetStateAction, useState } from 'react'
+import { SetStateAction, useEffect, useState } from 'react'
+import io, { Socket } from 'socket.io-client'
 
+const socket: Socket = io('http://localhost:8800');
+
+console.log(socket)
+
+interface MessageData {
+  data: string;
+  cord: [number, number];
+}
 
 const Map = () => {
 
 
       const [current, setCurrent] = useState('')
-      const [to, setTo] = useState('')
+      const [to, setTo] = useState<MessageData | null>(null);
+      const [room, setRoom] = useState('')
       
       const [currentCord, setCurrentCord] = useState<[number, number]>([3.393999,6.454811])
-      const [toCord, setToCord] = useState<[number, number]>([6.2747786,4.9334651])
 
       console.log(currentCord)
-      console.log(toCord)
 
-     
-      
       const getCurrent = async (e: { target: { value: SetStateAction<string> } }) => {
         setCurrent(e.target.value)
         const coordinates = await fetchCoordinates(current);
         setCurrentCord(coordinates);
       };
 
-      const getTo = async (e: { target: { value: SetStateAction<string> } }) => {
-        setTo(e.target.value)
-        const coordinates = await fetchCoordinates(to);
-        setToCord(coordinates);
-      };
+      const joinRoom = () =>{
+        if(room !== ""){
+          socket.emit("joinRoom",room)
+        }
+      }
+
+      const sendMessage =async()=>{
+        socket.emit('sendMessage',{
+          data:current,
+          cord:currentCord,
+          room
+        })
+      }
+
+      useEffect(()=>{
+        socket.on('receiveMessage',(data)=>{
+          setTo(data)
+          console.log(data)
+        })
+      },[socket])
 
   return (
     <div className='flex flex-col justify-center items-center gap-5'>
-        <div className='flex w-full justify-center gap-[20%]'>
+        <div className='flex flex-col w-full justify-center gap-[20%] px-10'>
             {/* Current Location */}
+            <div>
+              <p className='text-title2 font-semibold'>Key</p>
+              <div className='flex items-center gap-2'>
+                <input type="text" onChange={(e)=>setRoom(e.target.value)} />
+                <CircleIconButtons icon={<SendDiagonal/>} func={joinRoom}/>
+              </div>
+            </div>
             <div>
               <p className='text-title2 font-semibold'>Current</p>
               <div className='flex items-center gap-2'>
                 <input type="text" onChange={getCurrent} />
+                <CircleIconButtons icon={<SendDiagonal/>} func={sendMessage}/>
               </div>
             </div>
-            {/* Set Location */}
-            <div>
-              <p className='text-title2 font-semibold'>To</p>
-              <div className='flex items-center gap-2'>
-                <input type="text" onChange={getTo} />
-              </div>
-            </div>
+            
+            <p>{to?.data}</p>
         </div>
-        <CustomMap user1={currentCord} user2={toCord}/>
+        <CustomMap user1={currentCord} user2={to?.cord || [0, 0]} />
     </div>
   )
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,95 +1,35 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import nodemailer from 'nodemailer'
-import { Email } from './models/Email'
+import express from 'express';
+import dotenv from 'dotenv';
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
 
 dotenv.config()
-const app = express()
-const Port = process.env.PORT
+const port = process.env.PORT
+const app = express();
+const server = http.createServer(app)
 
-const mongoUrl = process?.env?.MONGO_URL
-
-// connect to mongo db
-if(!mongoUrl)
-    {console.log("error")}
-else{
-    mongoose.connect(mongoUrl)
-    mongoose.connection.on("connected",()=>console.log("connected to mongo db"))
-    mongoose.connection.on("error",(err)=>console.log(`errors: ${err}`))
-}
-
-// middleware
-app.use(express.json())
 app.use(cors())
 
-const transporter = nodemailer.createTransport({
-    service:'gmail',
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD,
-    },
-    });
-
-app.get('/',(req, res)=>{
-    res.send('hell world')
-})
-
-// send email
-app.post('/:email',async(req, res)=>{
-
-    try {
-        const email = req.params.email
-        const {receiverEmail, text, subject} = req.body
-
-        if (!email || !receiverEmail || !text || !subject) {
-            return res.status(400).json({ error: "Missing required field" });
-        }
-
-        const info = await transporter.sendMail({
-            from: email, // sender address
-            to: receiverEmail, // list of receivers
-            subject: subject, // Subject line
-            text: text, // plain text body
-            html: `<b>${text}</b>`, // html body
-        });
-
-        const saveEmail = await Email.create({email, receiverEmail, subject, text})
-        
-        res.status(200).json(saveEmail)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-    
-    
-})
-
-// get by email
-app.get('/user/:email', async(req, res) => {
-    const email = req.params.email
-    if(!email){
-        res.status(400).json({ error: "Missing required field" });
-    }else{
-        try {
-            const emails = await Email.find({ email: email}) 
-            res.status(200).json(emails);
-        } catch (error) {
-            res.status(500).json(error);
-        }
+const io = new Server(server,{
+    cors:{
+        origin: 'http://localhost:5173',
+        methods: ['GET','POST','HEAD','DELETE','UPDATE']
     }
 })
 
-// get specific email
-app.get('/:id', async(req, res) => {
-    try {
-        const email = await Email.findById(req.params.id);
-        res.status(200).json(email)
-    } catch (error) {
-        res.status(500).json(error)
-    }
+io.on('connection',(socket)=>{
+    console.log(`Connected to ${socket.id}`)
+
+    socket.on('joinRoom',(data)=>{
+        socket.join(data)
+    })
+
+    socket.on('sendMessage',(data)=>{
+        socket.to(data.room).emit('receiveMessage', data)
+    })
 })
 
-app.listen(Port, ()=>{
-    console.log(`listening on port http://localhost:${Port}` )
+server.listen(port,()=>{
+    console.log(`listening on port https://localhost:${port}`)
 })
